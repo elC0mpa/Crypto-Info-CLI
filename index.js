06088648896683983d7c33d3066dbcdc64646016
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2020-09-13 20:19:11 -0300

    Using coingecko.js file logic

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const figlet = require("figlet");
 let price = require("crypto-price");
 const clui = require("clui");
 const inquirer = require("./lib/inquirer");
+const coingecko = require("./lib/coingecko");
 let Table = require("cli-table3");
-
-const CoinGecko = require("coingecko-api");
-const CoinGeckoClient = new CoinGecko();
 
 clear();
 console.log(
@@ -15,3 +13,22 @@ console.log(
     figlet.textSync("Crypto Info Tool", { horizontalLayout: "full" })
   )
 );
+
+const currencies = async () => {
+  const info = await inquirer.askCurrencyInfo();
+  const currency = info.currency[0].toLowerCase();
+  const status = await new clui.Spinner("Please wait until data is available");
+  status.start();
+  const crypto_info = await coingecko.getCryptoCurrencyInfo(
+    currency,
+    "bitcoin"
+  );
+  status.stop();
+  console.log(crypto_info);
+};
+
+const main = async () => {
+  await currencies();
+};
+
+main();
