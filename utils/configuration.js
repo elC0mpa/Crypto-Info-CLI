ba3d29702cf5c741274660505bfbbf89cbebbb44
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-02-24 01:18:00 -0300

    Fixed problem related to module.exports

--- utils/configuration.js
+++ utils/configuration.js
@@ -5,56 +5,56 @@ const inquirer = require("./../lib/inquirer");
 const clui = require("clui");
 const coingecko = require("./../lib/coingecko");
 
-module.exports = {
-  configureFavoriteCryptos: async () => {
-    let favs = undefined;
-    if (configuration.has("favs")) {
-      favs = configuration.get("favs");
-      console.log("Favorite cryptos:");
-      console.log(chalk.yellowBright(favs));
-    }
-    const { action } = await inquirer.favoriteCryptosActions();
-    if (action === "Add new crypto") {
-      const status = await new clui.Spinner(
-        "Please wait until data is available"
+const configureFavoriteCryptos = async () => {
+  let favs = undefined;
+  if (configuration.has("favs")) {
+    favs = configuration.get("favs");
+    console.log("Favorite cryptos:");
+    console.log(chalk.yellowBright(favs));
+  }
+  const { action } = await inquirer.favoriteCryptosActions();
+  if (action === "Add new crypto") {
+    const status = await new clui.Spinner(
+      "Please wait until data is available"
+    );
+    status.start();
+    const { data: ids } = await coingecko.getCryptoCurrencyIDs();
+    status.stop();
+    const names = ids.map((id) => {
+      return id.name;
+    });
+    let repeat = true;
+    let indexOfSelected = undefined;
+    let cryptos = [];
+    while (repeat) {
+      const { crypto } = await inquirer.insertNewCrypto(names);
+      indexOfSelected = names.indexOf(crypto);
+      names.splice(indexOfSelected, 1);
+      const { id } = ids.splice(indexOfSelected, 1)[0];
+      cryptos.push(id);
+      const { option } = await inquirer.waitKeyPress(
+        "Would you like to add another crypto?"
       );
-      status.start();
-      const { data: ids } = await coingecko.getCryptoCurrencyIDs();
-      status.stop();
-      const names = ids.map((id) => {
-        return id.name;
-      });
-      let repeat = true;
-      let indexOfSelected = undefined;
-      let cryptos = [];
-      while (repeat) {
-        const { crypto } = await inquirer.insertNewCrypto(names);
-        indexOfSelected = names.indexOf(crypto);
-        names.splice(indexOfSelected, 1);
-        const { id } = ids.splice(indexOfSelected, 1)[0];
-        cryptos.push(id);
-        const { option } = await inquirer.waitKeyPress(
-          "Would you like to add another crypto?"
-        );
-        repeat = option;
-      }
-      cryptos = favs === undefined ? cryptos : [...favs, ...cryptos];
-      configuration.set("favs", cryptos);
-    } else if (action === "Delete existing crypto") {
-      let repeat = true;
-      let indexOfSelected = undefined;
-      while (repeat) {
-        const { crypto } = await inquirer.deleteNewCrypto(favs);
-        indexOfSelected = favs.indexOf(crypto);
-        favs.splice(indexOfSelected, 1);
-        const { option } = await inquirer.waitKeyPress(
-          "Would you like to remove another crypto?"
-        );
-        repeat = option;
-      }
-      configuration.set("favs", favs);
+      repeat = option;
     }
-  },
+    cryptos = favs === undefined ? cryptos : [...favs, ...cryptos];
+    configuration.set("favs", cryptos);
+  } else if (action === "Delete existing crypto") {
+    let repeat = true;
+    let indexOfSelected = undefined;
+    while (repeat) {
+      const { crypto } = await inquirer.deleteNewCrypto(favs);
+      indexOfSelected = favs.indexOf(crypto);
+      favs.splice(indexOfSelected, 1);
+      const { option } = await inquirer.waitKeyPress(
+        "Would you like to remove another crypto?"
+      );
+      repeat = option;
+    }
+    configuration.set("favs", favs);
+  }
+};
+module.exports = {
   configure: async () => {
     const { config } = await inquirer.showConfigMenu();
     if (config === "Currency") {
